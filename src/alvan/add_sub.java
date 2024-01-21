16de1b12969b3ec9f285fcce0aa3b50e05269bab
Alvan Cardozo <cardozoalvan@gmail.com>
2024-01-21 21:12:01 +0530

    commit

---
+++ src/alvan/add_sub.java
@@ -0,0 +1,27 @@
+package alvan;
+import java.util.Scanner;
+
+
+public class add_sub {
+	
+	public static void main(String [] args ) {
+		int a,b;
+		
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Enter the first number: ");
+		a = scan.nextInt();
+		
+		System.out.println("Enter the seconf number: ");
+		b = scan.nextInt();
+		
+		int result = addNum(a,b);
+		System.out.println("Sum: " + result);
+		
+	}
+	public static int addNum(int a,int b) {
+		int sum = a+b;
+		return sum;
+	}
+
+}
